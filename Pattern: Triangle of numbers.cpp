3ecf68ce98bc68c0b5e59474784c118a2a4eb2df
mayankwatts <47928731+mayankwatts@users.noreply.github.com>
2019-03-03 14:52:41 +0530

    Create Pattern: Triangle of numbers.cpp

---
+++ Pattern: Triangle of numbers.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin>>n;
+  for(int i=1;i<=n;i++)
+  { 
+
+    int l=i-1;
+    int count=0;
+    
+    for(int j=1;j<=(2*i-1);j++)
+    {
+      count++;
+      if(j==1)
+      {
+         for(int k=1;k<=(n-i);k++)
+       {
+        cout<<" ";
+       }
+      }
+      
+     if(count<=i)
+      { l++;
+          cout<<l;
+      }
+     if(count>i)
+      { l--;
+       cout<<l;}
+       
+    }
+    cout<<endl;
+  }
+  
+   return 0; 
+}
+
